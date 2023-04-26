941f87b4c870503ac0d7d1938751c2c6120f2ea7
AleesMP <alejandromperis@gmail.com>
2023-04-26 20:37:08 +0200

    instalar Tailwind CSS, elimiar componentes que no se usan y añadir primeros estilos

--- src/App.vue
+++ src/App.vue
@@ -70,81 +70,85 @@ const deleteMovieById = async (id) => {
 </script>
 
 <template>
-  <div>
-    <div>
-      <h1>Formulario</h1>
-      <div>
+  <div class="flex flex-col gap-8 text-white p-4">
+    <div class="flex flex-col gap-3">
+      <h1 class="text-3xl">Formulario</h1>
+      <div class="flex flex-col gap-2">
         <label for="titulo">Título</label>
-        <input v-model="titulo" name="titulo" type="text" required/>
+        <input class="bg-slate-600 rounded px-2 py-1" v-model="titulo" name="titulo" type="text" required/>
       </div>
-      <div>
+      <div class="flex flex-col gap-2">
         <label for="anio">Año</label>
-        <input v-model="anio" name="anio" type="number" required />
+        <input class="bg-slate-600 rounded px-2 py-1" v-model="anio" name="anio" type="number" required />
       </div>
-      <div>
+      <div class="flex flex-col gap-2">
         <label for="genero">Género</label>
-        <input v-model="genero" name="genero" type="text" required />
+        <input class="bg-slate-600 rounded px-2 py-1" v-model="genero" name="genero" type="text" required />
       </div>
-      <div>
+      <div class="flex flex-col gap-2">
         <label for="sinopsis">Sinopsis</label>
-        <textarea v-model="sinopsis" name="sinopsis"></textarea>
+        <textarea class="bg-slate-600 rounded px-2 py-1" v-model="sinopsis" name="sinopsis"></textarea>
       </div>
-      <button @click="submit">Enviar</button>
+      <button class="px-4 py-2 rounded font-semibold bg-slate-800 w-1/3 mx-auto" @click="submit">Enviar</button>
     </div>
-    <div>
-      <h1>Listado</h1>
-      <table v-if="movies">
-        <tr>
-          <th>UUID</th>
-          <th>Title</th>
-          <th>Year</th>
-          <th>Genre</th>
-          <th>Synopsis</th>
-          <th>Created_at</th>
-          <th>Updated_at</th>
-        </tr>
-        <tr v-for="movie in movies" :key="movie.id">
-          <td>{{ movie.id }}</td>
-          <td>{{ movie.title }}</td>
-          <td>{{ movie.year }}</td>
-          <td>{{ movie.genre }}</td>
-          <td>{{ movie.synopsis }}</td>
-          <td>{{ movie.created_at }}</td>
-          <td>{{ movie.updated_at }}</td>
-        </tr>
+    <div class="flex flex-col gap-3">
+      <h1 class="text-3xl">Listado</h1>
+      <table class="border-collapse table-auto" v-if="movies">
+        <thead>
+          <tr>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">UUID</th>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">Title</th>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">Year</th>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">Genre</th>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">Synopsis</th>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">Created_at</th>
+            <th class="border-b border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-200 text-left">Updated_at</th>
+          </tr>
+        </thead>
+        <tbody class="bg-white dark:bg-slate-800">
+          <tr v-for="movie in movies" :key="movie.id">
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.id }}</td>
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.title }}</td>
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.year }}</td>
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.genre }}</td>
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.synopsis }}</td>
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.created_at }}</td>
+            <td class="border-b border-slate-700 p-4 pl-8 text-slate-400">{{ movie.updated_at }}</td>
+          </tr>
+        </tbody>
       </table>
       <div v-else>No hay ninguna película.</div>
     </div>
-    <div>
-      <h1>Editar o eliminar una pelicula</h1>
-      <div>
+    <div class="flex flex-col gap-3">
+      <h1 class="text-3xl">Editar o eliminar una película</h1>
+      <div class="flex flex-col gap-3">
         <label for="selectedMovie">Selecciona una película</label>
-        <select name="selectedMovie" id="selectedMovie" v-model="selectedMovie">
+        <select class="bg-slate-600 rounded px-2 py-1" name="selectedMovie" id="selectedMovie" v-model="selectedMovie">
           <option v-for="movie in movies" :key="movie.id" :value="movie">{{ `${movie.title} (${movie.id})` }}</option>
         </select>
-        <div v-if="selectedMovie">
-          <div>
+        <div class="flex flex-col gap-3" v-if="selectedMovie">
+          <div class="flex flex-col gap-2">
             <label for="selectedMovieTitle">Título</label>
-            <input v-model="selectedMovie.title" name="selectedMovieTitle" type="text" required/>
+            <input class="bg-slate-600 rounded px-2 py-1" v-model="selectedMovie.title" name="selectedMovieTitle" type="text" required/>
           </div>
-          <div>
+          <div class="flex flex-col gap-2">
             <label for="selectedMovieYear">Año</label>
-            <input v-model="selectedMovie.year" name="selectedMovieYear" type="number" required />
+            <input class="bg-slate-600 rounded px-2 py-1" v-model="selectedMovie.year" name="selectedMovieYear" type="number" required />
           </div>
-          <div>
+          <div class="flex flex-col gap-2">
             <label for="selectedMovieGenre">Género</label>
-            <input v-model="selectedMovie.genre" name="selectedMovieGenre" type="text" required />
+            <input class="bg-slate-600 rounded px-2 py-1" v-model="selectedMovie.genre" name="selectedMovieGenre" type="text" required />
           </div>
-          <div>
+          <div class="flex flex-col gap-2">
             <label for="selectedMovieSynopsis">Sinopsis</label>
-            <textarea v-model="selectedMovie.synopsis" name="selectedMovieSynopsis"></textarea>
+            <textarea class="bg-slate-600 rounded px-2 py-1" v-model="selectedMovie.synopsis" name="selectedMovieSynopsis"></textarea>
           </div>
-          <button @click="update(selectedMovie)">Actualizar</button>
-          <button @click="deleteMovieById(selectedMovie.id)">Eliminar</button>
+          <div class="flex gap-8 justify-center">
+            <button class="px-4 py-2 rounded font-semibold bg-slate-800 w-1/3" @click="update(selectedMovie)">Actualizar</button>
+            <button class="px-4 py-2 rounded font-semibold bg-red-800 w-1/3" @click="deleteMovieById(selectedMovie.id)">Eliminar</button>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped></style>
