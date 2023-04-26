89e7f8a92312de5b244ea92db9ec8ee8020a1a20
AleesMP <alejandromperis@gmail.com>
2023-04-26 17:55:28 +0200

    añadir Create del CRUD

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,50 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import { supabaseClient } from './services/supabase'
+import { ref } from 'vue'
+import { supabase } from './services/supabase'
 
+let titulo = ref('')
+let anio = ref()
+let genero = ref('')
+let sinopsis = ref(null)
+
+const submit = async () => {
+  // Podemos validar los datos de una forma más compleja (opcional)
+
+  // Enviar los datos a nuestra base de datos (supabase)
+  const { data, error } = await supabase
+    .from('movies')
+    .insert({ title: titulo.value, year: anio.value, genre: genero.value, synopsis: sinopsis.value })
+    .select()
+
+  if (error) {
+    console.log(error)
+  }
+  
+  console.log('Se ha creado la siguiente movie -> ', data)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <h1>Formulario</h1>
+    <div>
+      <label for="titulo">Título</label>
+      <input v-model="titulo" name="titulo" type="text" required/>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <label for="anio">Año</label>
+      <input v-model="anio" name="anio" type="number" required />
+    </div>
+    <div>
+      <label for="genero">Género</label>
+      <input v-model="genero" name="genero" type="text" required />
+    </div>
+    <div>
+      <label for="sinopsis">Sinopsis</label>
+      <textarea v-model="sinopsis" name="sinopsis"></textarea>
+    </div>
+    <button @click="submit">Enviar</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
