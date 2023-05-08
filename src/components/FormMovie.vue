e82591318352b4e62c25105192f93063177a908f
AleesMP <alejandromperis@gmail.com>
2023-05-08 21:22:27 +0200

    Arreglar algunas referencias en los formularios y en el titulo de la página

--- src/components/FormMovie.vue
+++ src/components/FormMovie.vue
@@ -17,19 +17,19 @@ const movie = computed({
 <template>
   <div>
     <div class="flex flex-col gap-2">
-      <label for="titulo">Título</label>
+      <label for="title">Título</label>
       <input class="bg-slate-600 rounded px-2 py-1" v-model="movie.title" name="title" type="text" required />
     </div>
     <div class="flex flex-col gap-2">
-      <label for="anio">Año</label>
+      <label for="year">Año</label>
       <input class="bg-slate-600 rounded px-2 py-1" v-model="movie.year" name="year" type="number" required />
     </div>
     <div class="flex flex-col gap-2">
-      <label for="genero">Género</label>
+      <label for="genre">Género</label>
       <input class="bg-slate-600 rounded px-2 py-1" v-model="movie.genre" name="genre" type="text" required />
     </div>
     <div class="flex flex-col gap-2">
-      <label for="sinopsis">Sinopsis</label>
+      <label for="synopsis">Sinopsis</label>
       <textarea class="bg-slate-600 rounded px-2 py-1" v-model="movie.synopsis" name="synopsis"></textarea>
     </div>
   </div>
