46be31f2af588dcdb8aa5f5004319c6f6d5c7442
AleesMP <alejandromperis@gmail.com>
2023-05-18 22:39:38 +0200

    Añadir información de la movie en su página

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -3,6 +3,8 @@ import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { supabase } from '../services/supabase'
 
+const supabaseStorageUrl = import.meta.env.VITE_SUPABASE_STORAGE_URL
+
 const route = useRoute()
 const router = useRouter()
 
@@ -28,9 +30,19 @@ const fetchMovieBySlug = async (slug) => {
         }
     }
 }
+
+const goBack = () => {
+    router.push('/')
+}
 </script>
 <template>
-    <div v-if="movie">
-        <div>{{ movie.title }}</div>
+    <div v-if="movie" class="flex flex-col gap-4 text-white">
+        <div class="text-4xl">{{ movie.title }}</div>
+        <img class="w-64" :src="supabaseStorageUrl + movie.image" :alt="`${movie.title} - Imagen`">
+
+        <div class="flex gap-4">
+            <button class="px-4 py-2 rounded font-semibold bg-slate-600 w-1/3" @click="goBack">Ir atras</button>
+            <button class="px-4 py-2 rounded font-semibold bg-slate-800 w-1/3">Reservar</button>
+        </div>
     </div>
 </template>
