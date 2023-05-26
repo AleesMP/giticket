1398ca18b143655ad13f591bb7f6f1c0d6e60a7e
AleesMP <alejandromperis@gmail.com>
2023-05-26 17:47:59 +0200

    Añadidas funcionalidades para el deploy

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,5 @@
 <script setup>
+import logo from "../assets/logo.svg"
 </script>
 
 <template>
@@ -10,13 +11,8 @@
                     <!-- menu -->
                     <div class="flex space-x-4">
                         <!-- logo -->
-                        <a href="" class="flex items-center space-x-2 py-5 px-2">
-                            <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-6 w-6 text-white">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z">
-                                </path>
-                            </svg>
+                        <a href="/" class="flex items-center space-x-2 py-5 px-2">
+                            <img class="w-8 h-8 aspect-square" :src="logo" alt="Giticket Logo">
                             <span class="text-2xl font-extrabold text-white">GITICKET</span>
                         </a>
                     </div>
